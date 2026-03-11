4c32f9edcd0cc80b6c8b0a442034176361dc64c0
Charles Roth <croth@thedance.net>
2026-03-11 13:27:52 -0400

    Building new clean form of election importer

---
+++ php/src/Column.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace CharlesRothDotNet\ImportTools;
+
+class Column {
+   public const YEAR      =  0;
+   public const COUNTY    =  1;
+   public const REGION    =  2;
+   public const VOTEFOR   =  3;
+   public const NAME      =  4;
+   public const PARTY     =  5;
+   public const VOTES_C   =  6;
+   public const VOTES_D   =  7;
+   public const VOTES_R   =  8;
+   public const VOTES_O   =  9;
+   public const VOTES_T   = 10;
+   public const ORG       = 11;
+   public const OFFICE    = 12;
+   public const TERMLEN   = 13;
+   public const TERMCYCLE = 14;
+   public const PARTIAL   = 15;
+   public const SUBDIST   = 16;
+   public const DIST      = 17;
+   public const INCUMBENT = 18;
+   public const DIAG      = 19;
+   public const CACHE     = 20;
+}
