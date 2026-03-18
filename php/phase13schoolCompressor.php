26f1115223702f897f1312dd6c00db105bb21356
Charles Roth <croth@thedance.net>
2026-03-18 13:48:54 -0400

    Add school compressor

--- php/phase13schoolCompressor.php
+++ php/phase13schoolCompressor.php
@@ -12,34 +12,40 @@ require "vendor/autoload.php";
 $env  = new EnvFile("_env");
 $pdo  = PdoHelper::makePdo($env);
 
-$ic = new IncumbentCompressor($pdo);
+$ic  = new IncumbentCompressor($pdo);
+$moc = new MultiCountyOfficeCombiner($pdo);
 $years = $ic->getElectionDates();
 
 $schools = $ic->getUncompletedIdsFor("school");
 
 foreach ($schools as $school) {
-    if (! $ic->hasCompleteCountiesFor('school', $school)) {
-       echo "unready $school\n";
-    }
-    else {
-        echo "$school uncompleted but ready\n";
-        continue;
-       //---Select the winners of all of the county races.
-       $sql = "SELECT DISTINCT org, office, subdist, district, partial, termlen, incumbent, cycle, year "
-          . "    FROM v4elections WHERE org in ('cnty', 'cnty-com', 'town', 'town-cou') AND county=$county "
-          . "   ORDER BY year, org, office, district, subdist, incumbent";
-       $ic->markRaceWinners($sql);
+    if ($ic->hasCompleteCountiesFor('school', $school)) {
 
-       //---Layer each year's race winners "over top of" the existing incumbents, replacing them
-       //   with new incumbents as needed.
-       foreach ($years as $year) {
-          $sql = "SELECT DISTINCT org, office, district, subdist "
-             . "    FROM v4elections WHERE year='$year' "
-             . "     AND org IN ('cnty', 'cnty-com', 'town', 'town-cou') AND county=$county "
-             . "   ORDER BY org, office, district, subdist";
-          $ic->applyRaceWinnersToIncumbents($sql, $year);
-       }
+        //---For school districts that cross counties, combine (add up) the individual county election rows, into one row each.
+        foreach ($years as $year) {
+           $sql = "SELECT DISTINCT org, office, district, subdist, partial, termlen, incumbent, year FROM v4elections "
+                . " WHERE year='$year' "
+                . "   AND org='schl-cou' AND district=$school "
+              . " ORDER BY org, office, district, subdist, incumbent, name";
+           $moc->combine($sql);
+        }
 
-       $ic->setCompleted("county", $county);
+//       //---Select the winners of all of the county races.
+//       $sql = "SELECT DISTINCT org, office, subdist, district, partial, termlen, incumbent, cycle, year "
+//          . "    FROM v4elections WHERE org in ('cnty', 'cnty-com', 'town', 'town-cou') AND county=$county "
+//          . "   ORDER BY year, org, office, district, subdist, incumbent";
+//       $ic->markRaceWinners($sql);
+//
+//       //---Layer each year's race winners "over top of" the existing incumbents, replacing them
+//       //   with new incumbents as needed.
+//       foreach ($years as $year) {
+//          $sql = "SELECT DISTINCT org, office, district, subdist "
+//             . "    FROM v4elections WHERE year='$year' "
+//             . "     AND org IN ('cnty', 'cnty-com', 'town', 'town-cou') AND county=$county "
+//             . "   ORDER BY org, office, district, subdist";
+//          $ic->applyRaceWinnersToIncumbents($sql, $year);
+//       }
+//
+//       $ic->setCompleted("county", $county);
     }
 }
