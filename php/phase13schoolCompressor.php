2345579ef56ed9fedeb5f9660468c0634d8935a3
Charles Roth <croth@thedance.net>
2026-03-18 14:24:33 -0400

    Add school compressor

--- php/phase13schoolCompressor.php
+++ php/phase13schoolCompressor.php
@@ -31,22 +31,22 @@ foreach ($schools as $school) {
            $moc->combine($sql);
         }
 
-//       //---Select the winners of all of the county races.
-//       $sql = "SELECT DISTINCT org, office, subdist, district, partial, termlen, incumbent, cycle, year "
-//          . "    FROM v4elections WHERE org in ('cnty', 'cnty-com', 'town', 'town-cou') AND county=$county "
-//          . "   ORDER BY year, org, office, district, subdist, incumbent";
-//       $ic->markRaceWinners($sql);
-//
-//       //---Layer each year's race winners "over top of" the existing incumbents, replacing them
-//       //   with new incumbents as needed.
-//       foreach ($years as $year) {
-//          $sql = "SELECT DISTINCT org, office, district, subdist "
-//             . "    FROM v4elections WHERE year='$year' "
-//             . "     AND org IN ('cnty', 'cnty-com', 'town', 'town-cou') AND county=$county "
-//             . "   ORDER BY org, office, district, subdist";
-//          $ic->applyRaceWinnersToIncumbents($sql, $year);
-//       }
-//
-//       $ic->setCompleted("county", $county);
+       //---Select the winners
+       $sql = "SELECT DISTINCT org, office, subdist, district, partial, termlen, incumbent, cycle, year "
+          . "    FROM v4elections WHERE org = 'schl-cou' AND district=$school "
+          . "   ORDER BY year, org, office, district, subdist, incumbent";
+       $ic->markRaceWinners($sql);
+
+       //---Layer each year's race winners "over top of" the existing incumbents, replacing them
+       //   with new incumbents as needed.
+       foreach ($years as $year) {
+          $sql = "SELECT DISTINCT org, office, district, subdist "
+             . "    FROM v4elections WHERE year='$year' "
+             . "     AND org IN ('schl-cou') AND district=$school "
+             . "   ORDER BY org, office, district, subdist";
+          $ic->applyRaceWinnersToIncumbents($sql, $year);
+       }
+
+       $ic->setCompleted("school", $school);
     }
 }
