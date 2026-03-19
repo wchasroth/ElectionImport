3283e3157245f46260f3c722a84949679e7e322d
Charles Roth <croth@thedance.net>
2026-03-18 22:10:24 -0400

    Clean up district-as-int mess

--- php/phase13stateCompressor.php
+++ php/phase13stateCompressor.php
@@ -17,7 +17,7 @@ $years    = $ic->getElectionDates();
 
 $org = " org in "
      . "    ('us', 'us-vp', 'us-sen', 'us-hou', 'mi', 'mi-lt', 'mi-ag', 'mi-sos', 'mi-boe', 'mi-msu', 'mi-um', 'mi-wsu', "
-     . "     'crt-sup', 'crt-a', 'crt-c', 'crd-d', 'crt-p') ";
+     . "     'crt-sup', 'crt-a', 'crt-c', 'crt-d', 'crt-p') ";
 //$org = " org in "
 //   . "    ('crt-d') ";
 
