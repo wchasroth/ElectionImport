95b67d85cd4b833e23c40ffcf2c254847dcc1daf
Charles Roth <wchasroth@gmail.com>
2026-03-18 23:23:41 -0400

    @

--- SQL/12-v4completed.sql
+++ SQL/12-v4completed.sql
@@ -1,6 +1,6 @@
 DROP   TABLE IF EXISTS v4completed;
 CREATE TABLE           v4completed (
    type  varchar(10) NOT NULL DEFAULT '',  index(type),
-   id    int         NOT NULL DEFAULT  0,  index(district),
-   primary key (type, district)
+   id    int         NOT NULL DEFAULT  0,  index(id),
+   primary key (type, id)
 );
