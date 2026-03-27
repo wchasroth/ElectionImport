2fec8ef48c6226c663b6df07b24812bebdc0f6e8
Charles Roth <croth@thedance.net>
2026-03-27 14:54:10 -0400

    Create log table for deleted seats

--- SQL/15-deletedSeats.sql
+++ SQL/15-deletedSeats.sql
@@ -1,5 +1,7 @@
 DROP   TABLE IF EXISTS v4deletedSeats;
 CREATE TABLE           v4deletedSeats LIKE v4seats;
+ALTER  v4deletedSeats ADD COLUMN when datetime    NOT NULL DEFAULT '2000-01-01';
+ALTER  v4deletedSeats ADD COLUMN who  varchar(60) NOT NULL DEFAULT '';
 
 DROP   TABLE IF EXISTS v4deletedIncumbents;
 CREATE TABLE           v4deletedIncumbents LIKE v4incumbents;
