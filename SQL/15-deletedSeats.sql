615774e0ebaea40c8286a794761b9db6fc0e0d5a
Charles Roth <croth@thedance.net>
2026-03-27 14:51:15 -0400

    Create log table for deleted seats

---
+++ SQL/15-deletedSeats.sql
@@ -0,0 +1,5 @@
+DROP   TABLE IF EXISTS v4deletedSeats;
+CREATE TABLE           v4deletedSeats LIKE v4seats;
+
+DROP   TABLE IF EXISTS v4deletedIncumbents;
+CREATE TABLE           v4deletedIncumbents LIKE v4incumbents;
