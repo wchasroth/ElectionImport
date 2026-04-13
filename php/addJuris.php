11ddbc6d6ff74c1ccba71e46fe8a57a46e3cd857
Charles Roth <croth@thedance.net>
2026-04-13 12:22:44 -0400

    Make addJuris strip out 'Precinct N' from precinct_full_name column

--- php/addJuris.php
+++ php/addJuris.php
@@ -67,7 +67,8 @@ function isIgnorableCandidate(string $choiceName): bool {
 function isAmbiguousOffice (string $office): bool {
    $minimalOffice = extractMinimalOfficeName($office);
    return in_array($minimalOffice,
-      ["clerk", "trustee", "supervisor", "treasurer", "mayor", "council", "commissioner", "city commissioner", "city commission", "councilperson", "city council", "library"]);
+      ["clerk", "trustee", "supervisor", "treasurer", "mayor", "council", "commissioner", "city commissioner", "city commission",
+       "city councilperson", "councilperson", "city council", "library"]);
 }
 
 function mayNeedDisambiguating(string $office): bool {
