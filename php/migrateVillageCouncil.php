84c0b098acd771f2a6d7611843984b60bd308816
Charles Roth <croth@thedance.net>
2026-04-27 11:50:27 -0400

    Use MatchableNames when comparing imported vs existing officials names

--- php/migrateVillageCouncil.php
+++ php/migrateVillageCouncil.php
@@ -6,6 +6,7 @@ namespace CharlesRothDotNet\ElectionImport;
 
 use CharlesRothDotNet\Alfred\AlfredPDO;
 use CharlesRothDotNet\Alfred\Csv;
+use CharlesRothDotNet\Alfred\MatchableName;
 use CharlesRothDotNet\Alfred\SqlFields;
 use CharlesRothDotNet\Alfred\EnvFile;
 use CharlesRothDotNet\Alfred\PdoHelper;
@@ -16,8 +17,8 @@ require "vendor/autoload.php";
 //---migrateVillageCouncil.php:  Migrate new village council seats/incumbents from 'importer' to 'dmeditor'.
 //
 //   We keep TWO copies of the v4 mivoter database going at any one time:
-//      "importer"  (frequently, 'elections')
-//      "dmeditor"  (frequently, 'elections2')
+//      "importer"  (frequently, 'elections')   pdo1
+//      "dmeditor"  (frequently, 'elections2')  pdo2
 //
 //   There were some problems with the original parsing of village council seats.  When we fixed,
 //   we may end up with more village council seats.  We need to migrate only the NEW (not already in
@@ -27,6 +28,7 @@ $env  = new EnvFile("_env");
 $pdo1 = PdoHelper::makePdo($env);  // importer
 $pdo2 = new AlfredPDO($env->get('dbname2'), $env->get('dbuser'), $env->get('dbpw'));  // dmeditor
 
+//---Get ALL the village council seats from importer
 $sql = "SELECT s.id, s.org, s.district, s.termlen, s.termcycle, "
      . "       i.id AS iid, i.name, i.elected, i.party, i.votes_C, i.votes_D, i.votes_R, i.votes_O, i.votes_T, "
      . "       i.web, i.email, i.phone, i.address, i.num2elect, i.county, i.resigned, i.partial, "
@@ -39,18 +41,19 @@ echo "$sql\n";
 $result1 = $pdo1->run($sql);
 foreach ($result1->getRows() as $row) {
     $district = $row['district'];
-    $name     = addslashes($row['name']);
-    $sql = "SELECT s.id, i.id "
+    $name     = $row['name'];
+    $newName  = new MatchableName($name);
+    $sql = "SELECT s.id, i.id, i.name "
          . "  FROM v4seats           AS s "
          . "  LEFT JOIN v4incumbents AS i  ON (i.seat_id = s.id) "
          . " WHERE s.org='vil-cou' "
-         . "   AND s.district='$district' "
-         . "   AND i.name = '$name'" ;
+         . "   AND s.district='$district' ";
     $result2 = $pdo2->run($sql);
-    if ($result2->getRowCount() == 1) {
-        echo "Found dup: $district $name\n";
-    }
+    $existingNames = [];
+    foreach ($result2->getRows() as $existingRow)  $existingNames[] = new MatchableName($existingRow['name']);
+    $best = $newName->findBestMatch($existingNames, 2);
+    if ($best < 0) fwrite(STDERR, "Should add: $district $name\n");
     else {
-        fwrite(STDERR, "Should add: $district $name\n");
+        echo "Found dup: $district $name == " . $existingNames[$best]->getSimplifiedName() . "\n";
     }
 }
