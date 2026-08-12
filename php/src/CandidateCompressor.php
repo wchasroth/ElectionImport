f699ba8b6cb9903edacebd0e5bd4920b4eb6917e
Charles Roth <croth@thedance.net>
2026-08-12 14:47:24 -0400

    Start reporting cases 3,4,5 in detail

--- php/src/CandidateCompressor.php
+++ php/src/CandidateCompressor.php
@@ -206,7 +206,7 @@ class CandidateCompressor {
    function reportCase(string $text, array $elected) {
       $countyName = $this->michiganCounties->getName($elected['county']);
       $juris  = "";
-      $office = $elected['office'];
+      $office = $elected['office'] . " (" . $elected['district'] . ")";
       $org = $elected['org'];
       if      ($org === 'cnty-com')    $office = "commissioner";
       else if ($org === 'city')        $juris  = $this->getJurisName($elected['district']);
