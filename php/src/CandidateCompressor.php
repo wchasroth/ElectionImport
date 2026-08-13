3e9fb575bb23486b493ac130edaed1c0580e81fe
Charles Roth <croth@thedance.net>
2026-08-13 13:01:40 -0400

    When successfully importing election winners into v4candidates, mark the 'won' flag as true (1)

--- php/src/CandidateCompressor.php
+++ php/src/CandidateCompressor.php
@@ -212,8 +212,7 @@ class CandidateCompressor {
 
    function updateCandidateName(int $id, string $name): void {
       $sqlFields = new SqlFields(['name' => $name, 'won' => 1]);
-      $sqlFields->getSetFragment();
-      $sql = "UPDATE v4candidates SET " . $sqlFields->getUpdateFragment() . " WHERE id = $id";
+      $sql = "UPDATE v4candidates SET " . $sqlFields->getSetFragment() . " WHERE id = $id";
       $this->runQuery($sql);
    }
 
