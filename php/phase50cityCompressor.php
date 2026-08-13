0c175cb2b2c3a121e1c27bd366e2f46b47282f42
Charles Roth <croth@thedance.net>
2026-08-12 22:29:06 -0400

    Add phase50cityCompressor

---
+++ php/phase50cityCompressor.php
@@ -0,0 +1,45 @@
+#!/usr/bin/env php
+<?php
+declare(strict_types=1);
+
+namespace CharlesRothDotNet\ElectionImport;
+
+use CharlesRothDotNet\Alfred\EnvFile;
+use CharlesRothDotNet\Alfred\PdoHelper;
+
+require "vendor/autoload.php";
+
+$env  = new EnvFile("_env");
+$pdo  = PdoHelper::makePdo($env);
+
+$cc  = new CandidateCompressor($pdo);
+$moc = new MultiCountyOfficeCombiner($pdo);
+$year = "2026-08-04";
+
+$cities = $cc->getIdsFor("city");
+
+foreach ($cities as $city) {
+    if ($cc->hasCompleteCountiesFor('city', $city)) {
+
+        //---For cities that cross counties, combine (add up) the individual county election rows, into one row each.
+        $sql = "SELECT DISTINCT org, office, district, subdist, partial, termlen, incumbent, year FROM v4elections "
+             . " WHERE year='$year' "
+             . "   AND org IN ('city', 'city-cou') AND district='$city' "
+           . " ORDER BY org, office, district, subdist, incumbent, name";
+        $moc->combine($sql);
+
+       //---Select the winners
+       $sql = "SELECT DISTINCT org, office, subdist, district, partial, termlen, incumbent, cycle, year "
+          . "    FROM v4elections WHERE org IN ('city', 'city-cou') AND district='$city' "
+          . "   ORDER BY year, org, office, district, subdist, incumbent";
+       $cc->markRaceWinners($sql);
+
+       //---Layer each year's race winners "over top of" the existing incumbents, replacing them
+       //   with new incumbents as needed.
+       $sql = "SELECT DISTINCT org, office, district, subdist "
+          . "    FROM v4elections WHERE year='$year' "
+          . "     AND org IN ('city', 'city-cou') AND district='$city' "
+          . "   ORDER BY org, office, district, subdist";
+       $cc->applyRaceWinnersToIncumbents($sql, $year);
+    }
+}
